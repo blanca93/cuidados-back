4d593d5f448c9b8c1aead46069b5b02021037ba3
Blanca Sierra Izquierdo <blanca.sierra@neoris.com>
2022-10-03 20:33:32 +0200

    Primer commit

---
+++ src/main/java/com/prueba/cuidados/CuidadosApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.cuidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/cuidados/CuidadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.cuidados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
